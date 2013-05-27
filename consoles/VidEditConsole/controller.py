ab7eddd0a09d34ad2bf5ab61642ac5cf2d932e4c
Adam Mayer <me@phooky.name>
2013-05-26 22:04:50 -0400

    classified

--- consoles/VidEditConsole/controller.py
+++ consoles/VidEditConsole/controller.py
@@ -3,43 +3,57 @@ import serial.tools.list_ports as list_ports
 import time
 import struct
 
-ports={}
+illum_count = 25
 
-for (name,_,_) in list_ports.grep('/dev/ttyACM*'):
-    port = serial.Serial(name, timeout=3)
-    port.write('I\n')
-    teensyid = port.readline().strip()
-    ports[teensyid] = port
 
-for (i,p) in ports.items():
-    print("Found {0}".format(i))
 
-t=ports['teensy']
-tpp=ports['teensypp']
 
 illum_count = 25
 
-# imap entries are (pressed, mode)
-imap = [(False,0)]*illum_count
+class Controller:
+    def __init__(self):
+        ports={}
+        for (name,_,_) in list_ports.grep('/dev/ttyACM*'):
+            port = serial.Serial(name, timeout=3)
+            port.write('I\n')
+            teensyid = port.readline().strip()
+            ports[teensyid] = port
+        for (i,p) in ports.items():
+            print("Found {0}".format(i))
+        self.t=ports['teensy']
+        self.tpp=ports['teensypp']
+        # imap entries are (pressed, mode)
+        self.imap = [(False,0)]*illum_count
+        self.t.write('m\\x0cmBoot sequence\\ncomplete.\n')
+        time.sleep(0.5)
+        self.t.write('m\\x0c\n')
+        for i in range(illum_count):
+            self.t.write('i{0}:{1}\n'.format(i,0))
+    def get_keypresses(self):
+        ipressed = []
+        self.tpp.write('r\n')
+        keys = self.tpp.readline().strip()
+        for i in range(illum_count):
+            newp = keys[i]=='1'
+            (oldp,mode) = self.imap[i]
+            if (newp and not oldp):
+                # button down press
+                ipressed.append(i)
+            self.imap[i] = (newp,mode)
+        return ipressed
+    def set_illuminated(self,i,mode):
+        self.t.write('i{0}:{1}\n'.format(i,mode))
+        (oldp, _) = self.imap[i]
+        self.imap[i] = (oldp, mode)
+    def set_led(self,i,mode):
+        self.t.write('l{0}:{1}\n'.format(i,mode))
+    def send_msg(self,msg):
+        self.t.write('m{0}'.format(msg))
 
-for i in range(illum_count):
-    t.write('i{0}:{1}\n'.format(i,0))
-
-t.write('m\\x0cmBoot sequence\\ncomplete.\n')
-time.sleep(0.5)
-t.write('m\\x0c\n')
-
+c = Controller()
 while True:
     time.sleep(0.05)
-    tpp.write('r\n')
-    keys = tpp.readline().strip()
-    for i in range(illum_count):
-        newp = keys[i]=='1'
-        (oldp,mode) = imap[i]
-        if (newp and not oldp):
-            # button down press
-            mode = (mode + 1) % 5
-            t.write('i{0}:{1}\n'.format(i,mode))
-            t.write('m\\x0cBUTTON {0}\n'.format(i))
-            print('Button {0} pressed'.format(i))
-        imap[i] = (newp,mode)
+    for i in c.get_keypresses():
+        # button down press
+        mode = (c.imap[i][1] + 1) % 5
+        c.set_illuminated(i,mode)

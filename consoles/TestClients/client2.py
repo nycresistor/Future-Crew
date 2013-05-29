4280ecfa071760aaf2ffcd1e93ef60b3d41e8beb
Adam Mayer <me@phooky.name>
2013-05-29 18:00:19 -0400

    cleaning up example client

--- consoles/TestClients/client2.py
+++ consoles/TestClients/client2.py
@@ -1,11 +1,17 @@
 from future_client import FutureClient, Game, MessageSlot
 import time
 
+class PressGame(Game):
+    def __init__(self,name,message,button):
+        super(PressGame, self).__init__(name, message)
+
+pg1 = PressGame('pg1','PRESS BUTTON A','A')
+pg2 = PressGame('pg2','PRESS BUTTON B','B')
+
 if __name__ == '__main__':
     fc = FutureClient('ws://localhost:8888/socket','client2.py w/ lib')
     fc.available_games = [
-        Game('test1','MUST PRESS BUTTON'),
-        Game('test2','MUST PRESS BUTTON NOW',time=3.2)
+        pg1, pg2
         ]
     fc.message_slots = [
         MessageSlot(1,50)

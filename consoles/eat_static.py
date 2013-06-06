ef65ab359533d5ee7f30ea4ee4035a48ed44faa5
Adam Mayer <me@phooky.name>
2013-06-05 22:12:10 -0400

    fixing up eat_static trick

--- consoles/eat_static.py
+++ consoles/eat_static.py
@@ -3,12 +3,17 @@
 from subprocess import call
 import time
 
+
 def make_static():
-    call(['tvservice','--explicit="CEA 16"'])
+    call(['tvservice','--explicit=CEA 16'])
+
+def set_res(x,y):
+    call(['fbset', '-xres', str(x), '-yres', str(y)])
 
 def make_normal(x,y):
-    call(['tvservice','--sdtv="NTSC 4:3"'])
-    call(['fbset', '-xres', x, '-yres', y])
+    call(['tvservice','--sdtv=NTSC 4:3'])
+    set_res(x,y+1)
+    set_res(x,y)
 
 print "Let them eat static."
 make_static()

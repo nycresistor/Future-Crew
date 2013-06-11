10f2dccf3fdac945b6bf51a50eabcbd0e24d178a
Adam Mayer <me@phooky.name>
2013-06-11 19:50:26 -0400

    adding start/stop script

---
+++ server/startstop.py
@@ -0,0 +1,25 @@
+from websocket import create_connection, socket
+import json
+import time
+import threading
+from sys import argv
+
+urlstring = None
+
+if __name__=='__main__':
+    if not urlstring:
+        urlstring = getenv('SERVER_URL',"ws://localhost:2600/socket")
+    try:
+        socket = create_connection(urlstring,5.0)
+    except socket.error:
+        print "Could not connect to server. Trying again."
+        time.sleep(1.5)
+
+    msg = {}
+    if sys.argv[1] == 'start':
+        msg = {'a':'session_start'}
+        socket.send(json.dumps(msg))
+    elif sys.argv[1] == 'abort':
+        msg = {'a':'session_abort'}
+        socket.send(json.dumps(msg))
+        

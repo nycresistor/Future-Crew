59778fbeb8cfce99caa39215fd8f3308fe11448e
Trammell Hudson <hudson@osresearch.net>
2013-06-13 18:42:51 -0400

    switches game for patch console

--- consoles/PatchConsole/patch_controller.py
+++ consoles/PatchConsole/patch_controller.py
@@ -5,19 +5,21 @@ import time
 import struct
 import threading
 import random
+import patches
+from random import randint
 
 class Controller:
     def __init__(self):
 	self.cons = {}
-        self.port = serial.Serial("/dev/ttyACM0", timeout=3)
+        self.port = serial.Serial("/dev/tty.usbmodem480", timeout=3)
 
     def get_patches(self):
         keys = self.port.readline().strip()
 	if not keys:
 	    return
-	print keys
+	#print keys
 	cons = keys.split(' ')
-	self.switches = cons[0]
+	self.switches = int(cons[0], 16)
 	con_map = {}
 	for con in cons[1:]:
 		fromto = con.split(':')
@@ -47,18 +49,43 @@ class PatchAtoBGame(Game):
         self.thread = t
         t.start()
 
+class ToggleSwitchGame(Game):
+    def __init__(self,c):
+        super(ToggleSwitchGame, self).__init__('sw','Toggle switch')
+        self.c = c
+
+    def play_game(self):
+	self.sw_num = randint(0,6)
+	self.start_value = self.c.switches & (1 << self.sw_num)
+ 	sw_name = patches.switches[self.sw_num]
+	print sw_name, ": ", str(self.start_value)
+	self.update_message('Toggle ' + sw_name)
+	while 1:
+	    if (self.c.switches & (1 << self.sw_num) == self.start_value):
+		continue
+
+	    print "Success!"
+	    self.finish(5)
+	    return
+
+    def on_start(self):
+        t = threading.Thread(target = self.play_game)
+        self.thread = t
+        t.start()
+
 class StdoutSlot(MessageSlot):
     def __init__(self, c, id=None, length=40):
         self.c = c
         super(StdoutSlot, self).__init__(id,length)
 
     def on_message(self,text):
-	print "Message: ", text
+	print "M: ", text
 
 c = Controller()
 
 games = [
-    PatchAtoBGame(c),
+    #PatchAtoBGame(c),
+    ToggleSwitchGame(c),
 ]
 
 slots = [

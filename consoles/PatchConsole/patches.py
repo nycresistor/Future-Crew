dacdf498b21adac59f843101616f2a78212c1681
hollyhudson <hollyf@swcp.com>
2013-09-15 15:13:16 -0400

    Changed code reflect new simplified wiring.

--- consoles/PatchConsole/patches.py
+++ consoles/PatchConsole/patches.py
@@ -1,16 +1,16 @@
-verb_patches = {
-'0A': 'Invert', # 'Mid11',
-'0C': 'Convert', # 'Mid21',
-'19': 'Polarize', # 'Mid31',
-'1A': 'Adjust', # 'Mid41',
+#verb_patches = {
+#'0A': 'Invert', # 'Mid11',
+#'0C': 'Convert', # 'Mid21',
+#'19': 'Polarize', # 'Mid31',
+#'1A': 'Adjust', # 'Mid41',
 
-'0B': 'Replicate', # 'Mid12',
-'17': 'Modulate', # 'Mid22',
-'18': 'Decimate', # 'Mid32',
+#'0B': 'Replicate', # 'Mid12',
+#'17': 'Modulate', # 'Mid22',
+#'18': 'Decimate', # 'Mid32',
 
-'15': 'Reverse', # 'Mid13',
-'16': 'Purge', # 'Mid23',
-'09': 'Verify', # 'Mid33',
+#'15': 'Reverse', # 'Mid13',
+#'16': 'Purge', # 'Mid23',
+#'09': 'Verify', # 'Mid33',
 
 #'21': 'Adjust', # 'R11',
 #'01': 'Polarize', #'R32',
@@ -18,10 +18,9 @@ verb_patches = {
 #'02': 'Invert', # 'R22',
 #'00': 'Verify', # 'R23',
 
+#}
 
-}
-
-noun_patches = {
+noun_patches_rt = {
 '1D': 'Main servos', # 'Mid51',
 '1C': 'Secondary servos', # 'Mid61',
 '1E': 'Tertiary servos', # 'Mid71',
@@ -36,15 +35,22 @@ noun_patches = {
 '07': 'Lateral thruster', # 'Mid63',
 '05': 'Medial thruster', # 'Mid73',
 '03': 'Reverse thruster', # 'Mid83',
+}
 
-'11': '#1 frombulator',		# L 1,1
-'12': '#2 frombulator',		# L 1,2
-'13': '#3 frombulator',		# L 1,3
-'14': '#4 frombulator',		# L 1,4
-'10': '#1 tranmogrifier',	# L 2,1
-'0F': '#2 tranmogrifier',	# L 2,2
-'0E': '#3 tranmogrifier',	# L 2,3
-'0D': '#4 tranmogrifier',	# L 2,4
+noun_patches_lt = {
+'11': '#1 Frombulator',		# L 1,1
+'12': '#2 Frombulator',		# L 1,2
+'13': '#3 Frombulator',		# L 1,3
+'14': '#4 Frombulator',		# L 1,4
+'10': '#1 Tranmogrifier',	# L 2,1
+'0F': '#2 Tranmogrifier',	# L 2,2
+'0E': '#3 Tranmogrifier',	# L 2,3
+'0D': '#4 Tranmogrifier',	# L 2,4
+'22': '#1 Perturbulator',
+'00': '#2 Perturbulator',
+'21': '#4 Perturbulator',
+'01': '#1 Magnifier',
+'02': '#3 Magnifier',
 }
 
 switches = (
